8974a5a9c2867d3c36697bae58ca8068985e1a06
amanpreet8507 <105711554+amanpreet8507@users.noreply.github.com>
2022-07-21 02:57:25 -0700

    Assignment 2 2nd Commit
    List of names of movies on web page

--- Assignment 2/code.js
+++ Assignment 2/code.js
@@ -1,2 +1,21 @@
+function process_res(data){
+    console.log(data);
+    for( i = 0; i < data.results.length; i++){
+    $("#result").append(JSON.stringify(data.results[i].original_title) + "<br>")
+    }
+}
+
+function search_(){
+    movie_name = $("#movie_name").val()
+    $.ajax({
+        url: `https://api.themoviedb.org/3/search/movie?api_key=1b977ff48db9f4e31e40912f11c4ea6b&language=en-US&query=${movie_name}&page=1&include_adult=false`,
+        type: "get",
+        success: process_res
+    })
+}
+function setup () {
+    $("#find_movie_info").click(search_)
+}
+
 
 $(document).ready(setup)
