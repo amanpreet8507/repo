945f85da94bb4acb0823fe37a3283d89a7574a53
Amanpreet <amanpreet8507@gmail.com>
2022-07-22 10:26:13 -0700

    Assignment 2 done
    Assignment 3 started

--- Assignment 2/code.js
+++ Assignment 2/code.js
@@ -1,23 +1,36 @@
-function process_res(data){
-    console.log(data);
-    for( i = 0; i < data.results.length; i++){
-    $("#result").append(JSON.stringify(data.results[i].original_title) + "<br>")
-    $("#result").append(JSON.stringify(data.results[i].overview) + "<br><br><br>")
-    $("#result").append(` <img src = "https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">`)
+function process_response(data){
 
+    for( i = 0; i < data.results.length; i++){
+    $("#results").append(JSON.stringify(data.results[i].original_title) + "<br>")
+    $("#results").append(JSON.stringify(data.results[i].overview) + "<br><br><br>")
+    $("#results").append(` <img src = "https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">`)
+    
+    z = `<button id="${data.results[i].backdrop_path}"
+    class = "backdrop_button"> Backdrop image</button>`
+    $("#results").append(z + "<br>");
     }
 }
 
 function search_(){
-    movie_name = $("#movie_name").val()
+    w = $("#movie_name").val();
     $.ajax({
-        url: `https://api.themoviedb.org/3/search/movie?api_key=1b977ff48db9f4e31e40912f11c4ea6b&language=en-US&query=${movie_name}&page=1&include_adult=false`,
-        type: "get",
-        success: process_res
+        "url": `https://api.themoviedb.org/3/search/movie?api_key=1b977ff48db9f4e31e40912f11c4ea6b&language=en-US&query=${w}&page=1&include_adult=false`,
+        "type": "GET",
+        "success": process_response
     })
+}
+
+function display_back_drop() {
+    w = $(this).attr("id");
+    console.log(`<img src="https://image.tmdb.org/t/p/original${w}" width="100"%>`);
+    $("#right_div").html(`<img src="https://image.tmdb.org/t/p/original${w}" width="100%">`)
+
+
+
 }
 function setup () {
     $("#find_movie_info").click(search_)
+    $("body").on ("click",".backdrop_button", display_back_drop)
 }
 
 
---
+++ Assignment 3/code.js
@@ -0,0 +1,65 @@
+menu_value = ""
+data = ""
+page_number = 1
+page_size = ""
+function process_response(x) {
+    data = x
+    page_number = 1
+    display()
+}
+
+function call_ajax() {
+    w = $("#movie_name").val();
+    $.ajax({
+        "url": `https://api.themoviedb.org/3/search/movie?api_key=1b977ff48db9f4e31e40912f11c4ea6b&query=${w}`,
+        "type": "GET",
+        "success": process_response
+    })
+}
+function display_back_drop() {
+    w = $(this).attr("id");
+    console.log(`<img src="https://image.tmdb.org/t/p/original${w}" width="100"%>`);
+    $("#right_div").html(`<img src="https://image.tmdb.org/t/p/original${w}" width="100%">`)
+
+
+
+}
+
+function display() {
+    page_size = Number(page_size)
+    first_index = page_size * (page_number - 1)
+    last_index = page_size * (page_number - 1) + (page_size - 1)
+    for (i = first_index; i <= last_index; i++) {
+
+        $("#results").append(data.results[i].original_title + "<br>");
+
+        $("#results").append(data.results[i].overview + "<br>");
+        x = data.results[i].poster_path
+        image_html = `<img src='https://image.tmdb.org/t/p/w500/${x}'>`
+        $("#results").append(image_html + "<br>");
+
+        z = `<button id="${data.results[i].backdrop_path}" class="backdrop_button"> backdrop image!</button>`
+        $("#results").append(z + "<br>");
+    }
+}
+
+function chage_page_number() {
+    page_number = $(this).attr("id");
+    page_number = Number(page_number)
+    display()
+}
+function setup() {
+    $("#find_movie_info").click(call_ajax)
+    
+    $("body").on("click", ".backdrop_button", display_back_drop)
+ 
+     page_size = $("#page_size option:selected").val();
+     $("#page_size").change(function () {
+         page_size = $("#page_size option:selected").val();
+     })
+ 
+     $(".display").click(chage_page_number)
+ 
+ }
+ 
+$(document).ready(setup)
